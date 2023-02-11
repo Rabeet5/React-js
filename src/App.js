9ef67a88ae3c5c707ea7b167b70261d9f67ee056
Rabeet5 <rabeethowyawr@gmail.com>
2023-02-11 21:00:55 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      // let a = "Rabeet";
+      // let b = "Sati";
+      // let arr = ["is going lazy","He needs to do more"]
+      // let compile = a + "" + b + "" + arr;
+      // let texts = [{name:'rabeet',Lastname:"Sati"},
+      //   {name:"Sajid", Lastname:"Sati" }]     
+      // let arr = [{age:"18",Qualification:"Matric"}]  
+      
+
+
+
+return (
+<>
+<div>Hello World</div>
+
+
+
+
+
+
+
+
+
+
+
+{/* <div>
+  <ul>
+    {texts.map((item,index)=>{
+      // console.log(item,index)
+      return <li key={index}>{`${item.name} ${item.Lastname}`}</li>
+    })}
+  </ul>
+  <ul>
+    <li>{arr[0].age}</li>
+    <li>{arr[0].Qualification}</li> */}
+    {/* {arr.map((x,i)=>{ */}
+      {/* // console.log(x.Qualification)
+      // return 
+      // <li key={i}>{arr[0].age}</li> ;
+      // <li key={i}>{arr[0].Qualification}</li> ; */}
+      
+      
+    {/* // })} */}
+  {/* </ul>
+</div> */}
+ 
+</>
+
+    
+    
+    
+  )
 }
 
+
+// class App extends React.Component{
+//   render(){
+
+//     // JS Here !
+     
+//     let a = "Rabeet"
+//     let b = "sati"
+//     return(
+//       <>
+//         <div>
+//           <h1>{`${a} ${b}`}</h1>
+//         </div>
+//       </>
+//     )
+//   }
+// }
 export default App;
